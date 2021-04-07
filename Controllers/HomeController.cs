cfc5bb031d52a583b1ca6f686d3902d671c4d3e8
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-08 00:14:53 +0100

    Adding a LIst of claims

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace AuthBasics.Controllers
@@ -22,6 +23,13 @@ namespace AuthBasics.Controllers
 
         public IActionResult Authenticate()
         {
+            var grandmaClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "bob"),
+                new Claim(ClaimTypes.Email, "bob@fmail.com"),
+                new Claim("Grandma.Says", "Very nice boi."),
+            };
+
             return RedirectToAction("Index");
         }
     }
