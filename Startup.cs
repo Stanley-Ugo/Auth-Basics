53bd4351300a1d1e4c910b42d8842bef684f07e0
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-14 23:48:33 +0100

    Registering our CustomRequireClaimHandler for dependency Injection

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using AuthBasics.AuthorizationRequirements;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace AuthBasics
@@ -25,14 +27,28 @@ namespace AuthBasics
                 });
 
             services.AddAuthorization(config => {
-                var defaultAuthBuilder = new AuthorizationPolicyBuilder();
+                //var defaultAuthBuilder = new AuthorizationPolicyBuilder();
 
-                var defaultAuthPolicy = defaultAuthBuilder
-                    .RequireAuthenticatedUser()
-                    .Build();
+                //var defaultAuthPolicy = defaultAuthBuilder
+                //    .RequireAuthenticatedUser()
+                //    .RequireClaim(ClaimTypes.DateOfBirth)
+                //    .Build();
 
-                config.DefaultPolicy = defaultAuthPolicy;
+                //config.DefaultPolicy = defaultAuthPolicy;
+
+                //config.AddPolicy("Claim.COB", policyBuilder =>
+                //{
+                //    policyBuilder.RequireClaim(ClaimTypes.DateOfBirth);
+                //});
+
+                config.AddPolicy("Claim.COB", policyBuilder =>
+                {
+                    policyBuilder.AddRequirements(new CustomRequireClaim(ClaimTypes.DateOfBirth));
+                });
+
             });
+
+            services.AddScoped<IAuthorizationHandler, CustomRequireClaimHandler>();
 
             services.AddControllersWithViews();
         }
