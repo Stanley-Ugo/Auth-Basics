90fb14a1f52ca7959efd3b5a20f2e0e410b68baf
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-16 21:06:09 +0100

    Adding RazorPages() services in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,8 @@ namespace AuthBasics
             services.AddScoped<IAuthorizationHandler, CustomRequireClaimHandler>();
 
             services.AddControllersWithViews();
+
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
