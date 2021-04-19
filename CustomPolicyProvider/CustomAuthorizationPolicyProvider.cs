e51fdccdc82c96669c41ffd2c03109007855b3f2
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-19 23:59:34 +0100

    Getting the claims value

--- CustomPolicyProvider/CustomAuthorizationPolicyProvider.cs
+++ CustomPolicyProvider/CustomAuthorizationPolicyProvider.cs
@@ -9,8 +9,56 @@ namespace AuthBasics.CustomPolicyProvider
 {
     public static class DynamicPolicies
     {
+        public static IEnumerable<string> Get()
+        {
+            yield return SecurityLevel;
+            yield return Rank;
+        }
+
         public const string SecurityLevel = "SecurityLevel";
         public const string Rank = "Rank";
+    }
+
+    public static class DynamicAuthorizationPolicyFactory
+    {
+        public static AuthorizationPolicy Create(string policyName)
+        {
+            var parts = policyName.Split('.');
+            var type = parts.First();
+            var value = parts.Last();
+
+            switch (type)
+            {
+                case DynamicPolicies.Rank:
+                    return new AuthorizationPolicyBuilder()
+                        .RequireClaim("Rank", value)
+                        .Build();
+                case DynamicPolicies.SecurityLevel:
+                    return new AuthorizationPolicyBuilder()
+                        .AddRequirements(new SecurityLevelRequirements(Convert.ToInt32(value)))
+                        .Build();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class SecurityLevelRequirements : IAuthorizationRequirement
+    {
+        public int Level { get; }
+        public SecurityLevelRequirements(int level)
+        {
+            Level = level;
+        }
+    }
+
+    public class SecurityLevelHandler : AuthorizationHandler<SecurityLevelRequirements>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, SecurityLevelRequirements requirement)
+        {
+            var claimValue = context.User.Claims.FirstOrDefault(x => x.Type == DynamicPolicies.SecurityLevel).Value;
+            context.Succeed(requirement);
+        }
     }
     public class CustomAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
     {
@@ -20,6 +68,16 @@ namespace AuthBasics.CustomPolicyProvider
         }
         public override Task<AuthorizationPolicy> GetPolicyAsync(string policyName)
         {
+            foreach (var customPolicy in DynamicPolicies.Get())
+            {
+                if (policyName.StartsWith(customPolicy))
+                {
+                    var policy = new AuthorizationPolicyBuilder().Build();
+
+                    return Task.FromResult(policy);
+                }
+            }
+
             return base.GetPolicyAsync(policyName);
         }
     }
