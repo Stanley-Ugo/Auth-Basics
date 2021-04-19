ca3df3bb3a33f40e46c58882956de6a039d2c682
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-19 22:35:17 +0100

    Adding a Transformer folder

---
+++ Transformer/ClaimsTransformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthBasics.Transformer
+{
+    public class ClaimsTransformation
+    {
+    }
+}
