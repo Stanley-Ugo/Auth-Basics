8af47b456f3a8d9308891b0b5a033f80802d8161
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-19 02:15:02 +0100

    Commenting Cookie Login PAth"

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,6 @@ namespace AuthBasics
     public class Startup
     {
         // This method gets called by the runtime.
-
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
