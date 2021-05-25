2a54f96b886c2f4a53d2c45c6d2a422c78122c09
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-05-26 00:05:03 +0100

    New UserPricipal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -71,7 +71,7 @@ namespace AuthBasics.Controllers
 
             var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
 
-            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government")
+            var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
 
             var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
 
