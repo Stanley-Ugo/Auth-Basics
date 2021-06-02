b75c70dcaf4c2529be43ab2d27883660a56abbbb
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-06-02 04:42:32 +0100

    Adding Authorization

--- Controllers/OperationsController.cs
+++ Controllers/OperationsController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authorization.Infrastructure
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc
 using System;
 using System.Collections.Generic;
 using System.Linq;
